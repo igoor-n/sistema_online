9313940771bd8b0625e4b482d8a1611cebe23425
igoor-n <igoorn10@gmail.com>
2023-09-27 20:20:19 -0300

    atualizações

--- cadastro/models.py
+++ cadastro/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Curso(models.Model):
-    codigo = models.BigAutoField(primary_key=True)
+    codigo = models.AutoField(primary_key=True)
 
     nome=models.CharField(max_length=200,
                           null=False)
@@ -10,3 +10,16 @@ class Curso(models.Model):
     valor=models.DecimalField(max_digits=11,
                               decimal_places=2,
                               null=False)
+
+    def __str__(self):
+        return self.nome
+
+class Turma(models.Model):
+    curso = models.ForeignKey(Curso, on_delete=models.DO_NOTHING)
+    dataInicio = models.DateTimeField(null=False)
+    dataTermino = models.DateTimeField(null=False)
+
+    def __str__(self):
+        return f"{self.curso.nome} - {self.dataInicio} - {self.dataTermino}"
+
+
---
+++ cadastro/techinfo.py
@@ -0,0 +1,9 @@
+print(type(pessoa))
+pessoa["nome"] = "Luis"
+pessoa["e-mail"] = "luis@teste.com"
+pessoa["salário"] = "4040-4040"
+
+print(pessoa)
+
+novaPessoa = {"nome": "Gabi", "email": "gabi@teste.com", "salário": "5050-5050",  }
+print (novaPessoa)
